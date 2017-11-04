f4fb2fbe87beb30606e3ea762d749ba4168ac6b9
estevaocs <estevao_cs@hotmail.com>
2017-11-03 23:10:34 -0200

    Adicionando Asteroids
    Criando asteroides, animando a rotação no proprio eixo e programando sua destruiçao ao ser atingindo pelo tiro

---
+++ Assets/DestroyByContact.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag != "Boundary")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+        
+    }
+}
